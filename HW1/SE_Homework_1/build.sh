b717d3bed994566730d918ac95a2b2b576390ffe
Pizzi Andrea <pizzi.1995517@studenti.uniroma1.it>
2024-05-05 22:43:49 +0200

    + automatizzata disabilitazione randomizzazione degli indirizzi

--- HW1/SE_Homework_1/build.sh
+++ HW1/SE_Homework_1/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-if [cat tee /proc/sys/kernel/randomize_va_space == 2]; then
-	echo 0 | tee /proc/sys/kernel/randomize_va_space
+if [ $(cat /proc/sys/kernel/randomize_va_space) == 2 ]; then
+	echo "Rand address disabled"
+	echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
 fi
 podman build --tag debian:homework -f Dockerfile
